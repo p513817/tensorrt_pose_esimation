822a2a722ca0b02e5f888edf3d9ad6b962faff7b
MaxChangInnodisk <max_chang@innodisk.com>
2022-02-18 14:07:20 +0800

    fix: newest pip version could not install torch from html page and remove unused package in reuirements.sh

--- docker/install_trt_pose.sh
+++ docker/install_trt_pose.sh
@@ -1,49 +1,26 @@
 #!/bin/bash
+if [[ ! `dirname "$0"` == "docker" ]];then
+    echo "Move to docker/."
+    cd docker
+fi
 
-function find_py_pkg(){
-	PKG=$1
-	echo $(pip3 list | grep $PKG)
-}
-
-# init
-ROOT=`pwd`
-echo "Workspace is ${ROOT}" | boxes -p a1
+source ./format_print.sh
 
 # torch2trt
-printf "$(date +"%T") Check torch2trt ... "
-
+printd "$(date +"%T") Install torch2trt " Cy
 PKG="torch2trt"
-if [[ $(find_py_pkg $PKG) ]];then
-	echo "PASS!"
-else
-	echo "FAILED"
-	pip3 install setuptools packaging > /dev/null 2>&1
-	cd $ROOT
-	if [[ -n ${PKG} ]];then
-		git clone https://github.com/NVIDIA-AI-IOT/torch2trt
-	fi
-	cd ${PKG}
-	python3 setup.py install --plugins
-	cd $ROOT
-	rm -rf ${PKG}
-fi
+
+if [[ -n ${PKG} ]];then git clone https://github.com/NVIDIA-AI-IOT/torch2trt && cd ${PKG}; fi
+python3 setup.py -q install --plugins
+cd $ROOT && rm -rf ${PKG}
 
 # TRT_POSE
-printf "$(date +%T) Check trt_pose ... "
-
+printd "$(date +%T) Check trt_pose "
 TRG="pure_trt_pose"
 PKG="trt-pose"
-if [[ $(find_py_pkg $PKG) ]];then
-	echo "PASS"
-else
-	echo "FAILED"
-	if [[ -n ${TRG} ]];then
-		git clone https://github.com/p513817/pure_trt_pose.git
-	fi
-	cd ${TRG} 
-	python3 setup.py install
-	cd $ROOT
-	rm -rf ${TRG}
-fi
 
-echo "DONE"
+if [[ -n ${TRG} ]];then git clone https://github.com/p513817/pure_trt_pose.git && cd ${TRG} ; fi
+python3 setup.py -q install
+cd $ROOT && rm -rf ${TRG}
+
+printd "DONE" Cy
--- docker/requirements.sh
+++ docker/requirements.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-source format_print.sh
+if [[ -f format_print.sh ]];then
+	source format_print.sh
+else
+	function printd(){
+		echo $1
+	}
+fi
 
 function find_py_pkg(){
 	PKG=$1
@@ -9,7 +15,7 @@ function find_py_pkg(){
 printd "$(date +"%T") Initialize ... " Cy
 apt-get update -qqy
 apt-get install -qy figlet boxes tree > /dev/null 2>&1
-pip3 install --upgrade -q pip > /dev/null 2>&1
+pip3 install --force pip~=21.0.0
 
 ROOT=`pwd`
 echo "Workspace is ${ROOT}" | boxes -p a1
@@ -19,41 +25,13 @@ printd "$(date +"%T") Install OpenCV " Cy
 apt-get install -qqy ffmpeg libsm6 libxext6 #> /dev/null 2>&1
 pip3 install -q opencv-python==4.1.2.30 tqdm #> /dev/null 2>&1
 
-# TLT Converter
-# printd "$(date +"%T") Install Dependencies of TLT Converter " Cy
-# apt-get install -qqy libssl-dev #> /dev/null 2>&1
-# echo 'export TRT_LIB_PATH=/usr/lib/x86_64-linux-gnu' >> ~/.bashrc 
-# echo 'TRT_INC_PATH=/usr/include/x86_64-linux-gnu' >> ~/.bashrc 
-
-# iTAO requirements
-# printd "$(date +"%T") Install Dependencies of iTAO " Cy
-# apt-get install -y libxcb-xinerama0 qt5-default    
-# pip3 --disable-pip-version-check install PyQt5 pyqtgraph wget GPUtil
-
 # Torch 
 printd "$(date +"%T") Install torch, torchvision with CUDA 11.1 " Cy
-pip3 install -q torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 printd "Install other msicellaneous packages " Cy
 pip3 install -q tqdm cython pycocotools gdown setuptools packaging
 apt-get -qqy install python3-matplotlib bsdmainutils
-
-# torch2trt
-printd "$(date +"%T") Install torch2trt " Cy
-PKG="torch2trt"
-
-if [[ -n ${PKG} ]];then git clone https://github.com/NVIDIA-AI-IOT/torch2trt && cd ${PKG}; fi
-python3 setup.py -q install --plugins
-cd $ROOT && rm -rf ${PKG}
-
-# TRT_POSE
-printf "$(date +%T) Check trt_pose "
-TRG="pure_trt_pose"
-PKG="trt-pose"
-
-if [[ -n ${TRG} ]];then git clone https://github.com/p513817/pure_trt_pose.git && cd ${TRG} ; fi
-python3 setup.py -q install
-cd $ROOT && rm -rf ${TRG}
 
 echo -e "Done${REST}"
 # ---------------------------------------------------------------------------
